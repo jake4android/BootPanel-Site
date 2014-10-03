a10f50adb28933124e763d6a188d9e45fd7e622f
Comedyman937 <comedyman937@yahoo.com>
2014-10-03 12:30:52 -0400

    Fixed Download Spacing

--- download.php
+++ download.php
@@ -6,8 +6,8 @@
 				<center>
 					<h1>Download BootPanel</h1>
 					<p>
-						<a class="btn btn-lg btn-primary" href="https://github.com/BootPanel/BootPanel/archive/master.zip" role="button">Download ZIP</a></br>
-						<a class="btn btn-lg btn-primary disabled" href="" role="button">Download RPM</a></br>
+						<a class="btn btn-lg btn-primary" href="https://github.com/BootPanel/BootPanel/archive/master.zip" role="button">Download ZIP</a></br></br>
+						<a class="btn btn-lg btn-primary disabled" href="" role="button">Download RPM</a></br></br>
 					</p>
 				</center>
 			</div>
