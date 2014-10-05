3e988f7d2d115ae5c6f1565c2e9a3b5d2c19ccbe
Comedyman937 <comedyman937@yahoo.com>
2014-10-05 16:50:49 -0400

    Fixed Debuging Issue

--- index.php
+++ index.php
@@ -323,8 +323,7 @@ require 'conf/config.php';
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<script src="bootstrap/js/bootstrap.min.js"></script>
-		<script src="bootstrap/assets/js/docs.min.js"></script>
 		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-		<script src="bootstrap/assets/js/ie10-viewport-bug-workaround.js"></script>
+		<script src="bootstrap/js/ie10-viewport-bug-workaround.js"></script>
 	</body>
 </html>
