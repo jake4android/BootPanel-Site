47c023f96595ddd7a90f7008b13d0fd4044c5423
Comedyman937 <comedyman937@yahoo.com>
2014-10-08 13:31:36 -0400

    Minor NavBar Change

--- index.php
+++ index.php
@@ -44,11 +44,10 @@ require 'conf/config.php';
 								<span class="icon-bar"></span>
 								<span class="icon-bar"></span>
 							</button>
-							<a class="navbar-brand" href="">BootPanel</a>
+							<a class="navbar-brand" href="./">BootPanel</a>
 						</div>
 						<div class="navbar-collapse collapse">
 							<ul class="nav navbar-nav">
-								<li><a href="./">Home</a></li>
 								<li><a href="./?about">About</a></li>
 								<li><a href="./?contact">Contact</a></li>
 								<li><a href="./?api">API</a></li>
